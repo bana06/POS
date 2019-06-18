3e004b24968d709950fb4a522c8db84c408d9f19
bana06 <sabana.prilude@gmail.com>
2019-06-18 09:00:11 +0700

    popup

--- POS_cashier/app/src/main/java/com/sbe/pos_cashier/MainActivity.java
+++ POS_cashier/app/src/main/java/com/sbe/pos_cashier/MainActivity.java
@@ -4,6 +4,9 @@ import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.Dialog;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AlertDialog;
 import android.os.Bundle;
 import android.view.Menu;
@@ -12,22 +15,44 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
 import android.widget.SearchView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
+
+    //integrasi java dan xml
     ListView list_from_categori;
     ArrayAdapter<String>adapter;
+    Dialog dialogMain;
+    RelativeLayout customer;
 
-    public ImageButton imgbtn;
+    //utk relasi costumer java dan xml
+    TextView btn_exit;
+    LinearLayout btn_add_customer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         list_from_categori = (ListView)findViewById(R.id.list_from_categori);
+
+        //utk add customer
+        dialogMain = new Dialog(MainActivity.this);
+        customer = (RelativeLayout) findViewById(R.id.customer);
+
+        customer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showCustomer();
+            }
+        });
     }
+
     private void ShowDelete(){
         AlertDialog.Builder alert = new AlertDialog.Builder(this);
         alert.setTitle("Are you sure want to delete this order?")
@@ -48,6 +73,7 @@ public class MainActivity extends Activity {
                 alert.setCancelable(false);
         alert.show();
     }
+
     private void Showorder(){
         AlertDialog.Builder alert = new AlertDialog.Builder(this);
         alert.setTitle("Are you sure want to send this order to Kitchen?")
@@ -75,5 +101,29 @@ public class MainActivity extends Activity {
 
     public void Showorder(View view) {
         Showorder();
+    }
+
+    public void showCustomer(){
+        dialogMain.setContentView(R.layout.activity_costumer);
+
+        btn_add_customer = (LinearLayout) findViewById(R.id.btn_add_customer);
+        btn_exit = (TextView) findViewById(R.id.btn_exit);
+
+//        btn_add_customer.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Toast.makeText(MainActivity.this, "Ini button add customer", Toast.LENGTH_SHORT).show();
+//            }
+//        });
+//
+//        btn_exit.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                dialogMain.dismiss();
+//            }
+//        });
+
+        dialogMain.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialogMain.show();
     }
 }
